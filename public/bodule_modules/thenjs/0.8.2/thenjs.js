a80d045d592fca2b6732188f69286c1930addb88
island205 <island205@gmail.com>
2013-09-14 01:14:41 +0800

    more work

--- public/bodule_modules/thenjs/0.8.2/thenjs.js
+++ public/bodule_modules/thenjs/0.8.2/thenjs.js
@@ -1,34 +1,27 @@
-define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
-    'use strict';
-    /*global module, define, process*/
+define('thenjs@0.8.2/then', [], function (require, exports, module) {
+  
+  "use strict";
     
+    /*global module, define, process*/
     /*!
      * then.js, version 0.8.2, 2013/09/08
      * Another very small asynchronous promise tool!
      * https://github.com/teambition/then.js
      * MIT license, admin@zensh.com
      */
-    
-    (function () {
-        var slice = [].slice,
-            isArray = Array.isArray,
-            nextTick = typeof process === 'object' && isFunction(process.nextTick) ? process.nextTick : setTimeout;
-    
+    (function() {
+        var slice = [].slice, isArray = Array.isArray, nextTick = typeof process === "object" && isFunction(process.nextTick) ? process.nextTick : setTimeout;
         function isNull(obj) {
-            return obj === null || typeof obj === 'undefined';
+            return obj === null || typeof obj === "undefined";
         }
-    
         function isFunction(fn) {
-            return typeof fn === 'function';
+            return typeof fn === "function";
         }
-    
         function getError(obj, method, type) {
-            return new Error('Argument ' + obj + ' in "' + method + '" function is not a ' + type + '!');
+            return new Error("Argument " + obj + ' in "' + method + '" function is not a ' + type + "!");
         }
-    
         function each(defer, array, iterator, context) {
             var i, end, total, resultArray = [];
-    
             function next(index, err, result) {
                 total -= 1;
                 resultArray[index] = result;
@@ -36,12 +29,11 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                     defer(err || null, resultArray);
                 }
             }
-    
             next._this_then = true;
             if (!isArray(array)) {
-                defer(getError(array, 'each', 'array'));
+                defer(getError(array, "each", "array"));
             } else if (!isFunction(iterator)) {
-                defer(getError(iterator, 'each', 'function'));
+                defer(getError(iterator, "each", "function"));
             } else {
                 total = end = array.length;
                 if (!total) {
@@ -53,11 +45,8 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                 }
             }
         }
-    
         function eachSeries(defer, array, iterator, context) {
-            var end, i = -1,
-                resultArray = [];
-    
+            var end, i = -1, resultArray = [];
             function next(err, result) {
                 resultArray[i] = result;
                 i += 1;
@@ -68,12 +57,11 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                     defer(err || null, resultArray);
                 }
             }
-    
             next._this_then = true;
             if (!isArray(array)) {
-                defer(getError(array, 'eachSeries', 'array'));
+                defer(getError(array, "eachSeries", "array"));
             } else if (!isFunction(iterator)) {
-                defer(getError(iterator, 'eachSeries', 'function'));
+                defer(getError(iterator, "eachSeries", "function"));
             } else {
                 end = array.length;
                 if (!end) {
@@ -83,10 +71,8 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                 }
             }
         }
-    
         function parallel(defer, array, context) {
             var i, end, total, resultArray = [];
-    
             function next(index, err, result) {
                 total -= 1;
                 resultArray[index] = result;
@@ -94,10 +80,9 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                     defer(err || null, resultArray);
                 }
             }
-    
             next._this_then = true;
             if (!isArray(array)) {
-                defer(getError(array, 'parallel', 'array'));
+                defer(getError(array, "parallel", "array"));
             } else {
                 total = end = array.length;
                 if (!total) {
@@ -107,17 +92,14 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                         if (isFunction(array[i])) {
                             array[i].call(context, next.bind(null, i), i);
                         } else {
-                            defer(getError(array[i], 'parallel', 'function'));
+                            defer(getError(array[i], "parallel", "function"));
                         }
                     }
                 }
             }
         }
-    
         function series(defer, array, context) {
-            var end, i = -1,
-                resultArray = [];
-    
+            var end, i = -1, resultArray = [];
             function next(err, result) {
                 resultArray[i] = result;
                 i += 1;
@@ -125,17 +107,16 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                     if (isFunction(array[i])) {
                         array[i].call(context, next, i);
                     } else {
-                        defer(getError(array[i], 'series', 'function'));
+                        defer(getError(array[i], "series", "function"));
                     }
                 } else {
                     delete resultArray[-1];
                     defer(err || null, resultArray);
                 }
             }
-    
             next._this_then = true;
             if (!isArray(array)) {
-                defer(getError(array, 'series', 'array'));
+                defer(getError(array, "series", "array"));
             } else {
                 end = array.length;
                 if (!end) {
@@ -145,22 +126,16 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                 }
             }
         }
-    
         function thenjs(startFn, context) {
-            var fail = [],
-                Promise = function () {},
-                prototype = Promise.prototype;
-    
+            var fail = [], Promise = function() {}, prototype = Promise.prototype;
             function promiseFactory(fn, context) {
-                var promise = new Promise(),
-                    defer = promise.defer.bind(promise);
+                var promise = new Promise(), defer = promise.defer.bind(promise);
                 defer._this_then = promise;
                 fn(defer, context);
                 return promise;
             }
-    
             function tryNextTick(defer, fn) {
-                nextTick(function () {
+                nextTick(function() {
                     try {
                         fn();
                     } catch (error) {
@@ -168,40 +143,36 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                     }
                 });
             }
-    
             function eachAndSeriesFactory(fn) {
-                return function (array, iterator, context) {
-                    return promiseFactory(function (defer) {
+                return function(array, iterator, context) {
+                    return promiseFactory(function(defer) {
                         tryNextTick(defer, fn.bind(null, defer, array, iterator, context));
                     });
                 };
             }
-    
             function parallelAndSeriesFactory(fn) {
-                return function (array, context) {
-                    return promiseFactory(function (defer) {
+                return function(array, context) {
+                    return promiseFactory(function(defer) {
                         tryNextTick(defer, fn.bind(null, defer, array, context));
                     });
                 };
             }
-    
             function createHandler(defer, handler) {
-                return isFunction(handler) ? (handler._this_then ? handler : handler.bind(null, defer)) : null;
+                return isFunction(handler) ? handler._this_then ? handler : handler.bind(null, defer) : null;
             }
-    
-            prototype.all = function (allHandler) {
-                return promiseFactory(function (defer, self) {
+            prototype.all = function(allHandler) {
+                return promiseFactory(function(defer, self) {
                     self._all = createHandler(defer, allHandler);
                 }, this);
             };
-            prototype.then = function (successHandler, errorHandler) {
-                return promiseFactory(function (defer, self) {
+            prototype.then = function(successHandler, errorHandler) {
+                return promiseFactory(function(defer, self) {
                     self._success = createHandler(defer, successHandler);
                     self._error = createHandler(defer, errorHandler);
                 }, this);
             };
-            prototype.fail = function (errorHandler) {
-                return promiseFactory(function (defer, self) {
+            prototype.fail = function(errorHandler) {
+                return promiseFactory(function(defer, self) {
                     self._fail = createHandler(defer, errorHandler);
                     self._success = defer.bind(defer, null);
                     if (self._fail) {
@@ -209,35 +180,35 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                     }
                 }, this);
             };
-            prototype.each = function (array, iterator, context) {
-                return promiseFactory(function (defer, self) {
-                    self._each = function (dArray, dIterator, dContext) {
+            prototype.each = function(array, iterator, context) {
+                return promiseFactory(function(defer, self) {
+                    self._each = function(dArray, dIterator, dContext) {
                         each(defer, array || dArray, iterator || dIterator, context || dContext);
                     };
                 }, this);
             };
-            prototype.eachSeries = function (array, iterator, context) {
-                return promiseFactory(function (defer, self) {
-                    self._eachSeries = function (dArray, dIterator, dContext) {
+            prototype.eachSeries = function(array, iterator, context) {
+                return promiseFactory(function(defer, self) {
+                    self._eachSeries = function(dArray, dIterator, dContext) {
                         eachSeries(defer, array || dArray, iterator || dIterator, context || dContext);
                     };
                 }, this);
             };
-            prototype.parallel = function (array, context) {
-                return promiseFactory(function (defer, self) {
-                    self._parallel = function (dArray, dContext) {
+            prototype.parallel = function(array, context) {
+                return promiseFactory(function(defer, self) {
+                    self._parallel = function(dArray, dContext) {
                         parallel(defer, array || dArray, context || dContext);
                     };
                 }, this);
             };
-            prototype.series = function (array, context) {
-                return promiseFactory(function (defer, self) {
-                    self._series = function (dArray, dContext) {
+            prototype.series = function(array, context) {
+                return promiseFactory(function(defer, self) {
+                    self._series = function(dArray, dContext) {
                         series(defer, array || dArray, context || dContext);
                     };
                 }, this);
             };
-            prototype.defer = function (err) {
+            prototype.defer = function(err) {
                 this._error = this._fail ? fail.shift() : this._error;
                 if (this._all) {
                     try {
@@ -262,27 +233,25 @@ define('thenjs@0.8.2/then.js', [], function (require, exports, module) {
                     }
                 }
             };
-    
             thenjs.each = eachAndSeriesFactory(each);
             thenjs.eachSeries = eachAndSeriesFactory(eachSeries);
             thenjs.parallel = parallelAndSeriesFactory(parallel);
             thenjs.series = parallelAndSeriesFactory(series);
-    
-            return promiseFactory(function (defer) {
+            return promiseFactory(function(defer) {
                 tryNextTick(defer, isFunction(startFn) ? startFn.bind(context, defer) : defer);
             });
         }
-    
-        if (typeof module !== 'undefined' && module.exports) {
+        if (typeof module !== "undefined" && module.exports) {
             module.exports = thenjs;
-        } else if (typeof define === 'function') {
-            define(function () {
+        } else if (typeof define === "function") {
+            define(function() {
                 return thenjs;
             });
         }
-        if (typeof window === 'object') {
+        if (typeof window === "object") {
             window.then = thenjs;
         }
         return thenjs;
-    })();    
-});define('thenjs@0.8.2/thenjs', ['thenjs@0.8.2/then.js'], function (require, exports, module) {module.exports = require('thenjs@0.8.2/then.js');})
+    })();
+});
+define('thenjs@0.8.2/thenjs', ['thenjs@0.8.2/then.js'], function (require, exports, module) {module.exports = require('thenjs@0.8.2/then.js');})
